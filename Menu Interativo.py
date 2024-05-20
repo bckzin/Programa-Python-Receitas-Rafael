9012eb89672d3e962c1bc7ff78ad33372147e18d
Gabriel Araujo <gabs@cesar.school>
2024-05-20 15:25:35 -0300

    first commit

---
+++ Menu Interativo.py
@@ -0,0 +1,18 @@
+file = open("BancoDados.txt","a")
+cadastros = {}
+
+print("""
+---------------------------------
+Gerenciamento de Receitas
+---------------------------------
+
+OPÇÕES:
+[1] - ADICIONAR
+[2] - VISUALIZAR
+[3] - ATUALIZAR
+[4] - DELETAR
+
+[5] - SALVAR
+[6] - SAIR
+
+""")
